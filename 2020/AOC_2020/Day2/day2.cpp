3738a63f71c20e2c6e06a5d43dab003cf74b3fa7
HangmansMoose <glenwyntheeldar@gmail.com>
2020-12-25 11:21:13 +1100

    tokenizer

--- 2020/AOC_2020/Day2/day2.cpp
+++ 2020/AOC_2020/Day2/day2.cpp
@@ -9,13 +9,13 @@ int main()
 	std::ifstream file;
 	file.open("../../day2.txt");
 
-	std::vector<std::string> init_stream;
+	std::vector<std::string>* init_stream;
 
-	std::vector<std::string> range;
+	std::vector<std::string>* range;
 
-	std::vector<char> letter;
+	std::vector<char>* letter;
 
-	std::vector<std::string> passwords;
+	std::vector<std::string>* passwords;
 
 	std::vector<bool> rule_match;
 
@@ -27,14 +27,26 @@ int main()
 	{
 		std::getline(file, input_string);
 
-		init_stream.push_back(input_string);
+		init_stream->push_back(input_string);
 
 		std::cout << input_string << std::endl;
 
-
 	}
 
+
+	std::cout << init_stream->size() << std::endl;
 	
 
 	
+}
+
+
+
+bool tokenizer(int iter, std::vector<std::string>* arr, std::vector<std::string>* range, std::vector<std::string>* passwords,
+				std::vector<char>* letter, std::vector<bool>* rule_match)
+{
+	for (int i = 0; i < arr->size(); i++)
+	{
+
+	}
 }
