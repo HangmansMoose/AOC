567319953f3198d424892d6507e2bce058f748d9
HangmansMoose <hangmansmoose@gmail.com>
2020-12-26 23:34:52 +1100

    Day 2 still
    Built tokenizer

--- 2020/AOC_2020/Day2/day2.cpp
+++ 2020/AOC_2020/Day2/day2.cpp
@@ -9,44 +9,70 @@ int main()
 	std::ifstream file;
 	file.open("../../day2.txt");
 
-	std::vector<std::string>* init_stream;
+	//std::vector<std::string> init_stream;
 
-	std::vector<std::string>* range;
+	std::vector<std::string> range;
 
-	std::vector<char>* letter;
+	std::vector<char> letter;
 
-	std::vector<std::string>* passwords;
+	std::vector<std::string> passwords;
 
-	std::vector<bool> rule_match;
+	//std::vector<bool> rule_match;
 
 	std::string input_string;
 
+	int valid = 0;
+
 	
+
+	if (!file)
+	{
+		std::cout << "Could Not Open File" << std::endl;
+		return 0;
+	}
 
 	while (file.good())
 	{
 		std::getline(file, input_string);
 
-		init_stream->push_back(input_string);
+
+		if (tokenizer(input_string, range, passwords, letter))
+		{
+			valid++;
+		}
+
+		
 
 		std::cout << input_string << std::endl;
 
 	}
 
+	//std::cout << init_stream.size() << std::endl;
 
-	std::cout << init_stream->size() << std::endl;
 	
+	//monkey nuts
 
 	
 }
 
 
 
-bool tokenizer(int iter, std::vector<std::string>* arr, std::vector<std::string>* range, std::vector<std::string>* passwords,
-				std::vector<char>* letter, std::vector<bool>* rule_match)
+bool tokenizer(std::string input_string, std::vector<std::string>& range, std::vector<std::string>& passwords,
+	std::vector<char>& letter)
+
 {
-	for (int i = 0; i < arr->size(); i++)
-	{
+	char* token;
+	char* str= new char[input_string.size() + 1];
+	std::copy(input_string.begin(), input_string.end(), str);
+	str[input_string.size()] = '\0'; // don't forget the terminating 0
 
-	}
+	token = std::strtok(str, " ");
+
+	// don't forget to free the string after finished using it
+	delete[] str;
+	return false;
 }
+	
+
+
+
